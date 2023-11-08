25c6ae2ec123c82a9a17a1db605d27a4f66649ea
sammanamgain <lionalmessi609@gmail.com>
2023-11-08 11:50:57 +0545

    fix import error

--- MERN=REALESTATE/src/App.jsx
+++ MERN=REALESTATE/src/App.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { BrowserRouter,Routes,Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Signin from './pages/signin'
-import Signup from './pages/signup'
-import About from './pages/about'
-import Profile from './pages/profile'
-import Header from './Components/Header'
-import PrivateRoute from './Components/privateRoute'
-import CreateListing from './pages/CreateListing'
-import UpdatListing from './pages/UpdatListing'
-import Listing from './pages/Listing';
-import Search from './pages/Search'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./Components/Header";
+import PrivateRoute from "./Components/PrivateRoute";
+import CreateListing from "./pages/CreateListing";
+import UpdatListing from "./pages/UpdatListing";
+import Listing from "./pages/Listing";
+import Search from "./pages/Search";
 export default function App() {
   return (
     <BrowserRouter>
