83897c0d8205bdc638a294ba30d8a0ebff52c911
sammanamgain <lionalmessi609@gmail.com>
2023-11-08 11:40:06 +0545

    for deployment

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const cookieParser = require('cookie-parser');
 const { mongoose } = require("mongoose");
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
 require('dotenv').config({ path: './../.env' });
 const user_router = require('./routes/user_routes');
 const auth_router = require('./routes/auth_routes');
@@ -18,10 +19,16 @@ mongoose
     .catch(e => {
         console.log(e);
     });
+
+const __dirname1 = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/user', user_router);
 app.use('/api/auth', auth_router);
 app.use('/api/listing', listing_router);
-
+app.use(express.static(path.join(__dirname1, '/MERN=REALESTATE/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname1, 'MERN=REALESTATE', 'dist', 'index.html'));
+})
 app.listen('3000', 'localhost', () => { console.log('listening on 3000 port') })
