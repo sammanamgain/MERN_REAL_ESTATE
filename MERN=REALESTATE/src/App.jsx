c420b57f22dc4a34538fd31d6d80e6d938c14b1b
sammanamgain <lionalmessi609@gmail.com>
2023-11-08 13:08:20 +0545

    updated home route mistake

--- MERN=REALESTATE/src/App.jsx
+++ MERN=REALESTATE/src/App.jsx
@@ -17,6 +17,7 @@ export default function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<Signup />} />
         <Route path="/about" element={<About />} />
